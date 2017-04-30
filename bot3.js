ec7653e9a09209b5d96c2b90bde88bae01ef3f7a
Silvio Heinze <mail@silvioheinze.eu>
2017-04-30 11:55:16 +0200

    bot3 extended logging

--- bot3.js
+++ bot3.js
@@ -6,9 +6,9 @@ var T = new Twit(config);
 // We create a function to send out tweets mentioning how much we love CDN
 function cdnLove() {
 	// tell Twitter to post a new tweet with the following status
-	T.post('statuses/update', { status: '@CDN_EE I love you! <3' }, function(err, data, response) {
+	T.post('statuses/update', { status: '@CDN_EE I love you! <3' }, function(error, data, response) {
 		// log out any errors and responses
-	  console.log(data)
+	  console.log(error, data, response)
 	})
 }
 
